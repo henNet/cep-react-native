9b1cc5e0f2a1b46a84259a028122e31e49701dfa
henNet <dmf.n123@gmail.com>
2024-05-16 21:14:50 -0300

    Projeto de conculta de CEP

--- App.js
+++ App.js
@@ -1,5 +1,84 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "./style.js";
 
-export default function App() {
-  return <Text>Hello World</Text>;
+function App() {
+  const [cep, setCep] = useState("");
+  const [result, setResult] = useState("");
+
+  async function ConsultarCep() {
+    try {
+      const req = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+      const res = await req.json();
+
+      console.log(res);
+      var result =
+        "Cep: " +
+        res.cep +
+        "\n" +
+        "Logradouro: " +
+        res.logradouro +
+        "\n" +
+        "Complemento: " +
+        res.complemento +
+        "\n" +
+        "Bairro: " +
+        res.bairro +
+        "\n" +
+        "Localidade: " +
+        res.localidade +
+        "\n" +
+        "UF: " +
+        res.uf;
+      setResult(result);
+    } catch {
+      setResult("CEP não encontrado!");
+    }
+  }
+
+  function LimparCep() {
+    setCep("");
+    setResult("");
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.header}>
+        <Image source={require("./assets/favicon.png")} />
+        <Text style={styles.title}>Consulta de Ceps</Text>
+      </View>
+      {/*Form*/}
+      <View>
+        <TextInput
+          value={cep}
+          keyboardType="numeric"
+          placeholder="Digite um cep (só números)"
+          style={styles.InputText}
+          onChangeText={(e) => setCep(e.trim())}
+        />
+        <Button title="Consultar" onPress={ConsultarCep} />
+        <TouchableOpacity onPress={LimparCep}>
+          <Text style={styles.btnClear}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/*Resultado*/}
+      <View style={styles.result}>
+        <Text style={styles.result}>Resultado da busca:</Text>
+        <Text>{result}</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+export default App;
---
+++ style.js
@@ -0,0 +1,46 @@
+export const styles = {
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    paddingTop: 50,
+    gap: 10,
+  },
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#70ca2d",
+  },
+
+  InputText: {
+    backgroundColor: "#ececec",
+    width: 230,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+
+  result: {
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+
+  btnClear: {
+    marginTop: 5,
+    padding: 8,
+    textAlign: "center",
+    backgroundColor: "#bfbcbc",
+    borderRadius: 5,
+  },
+};
